dd6224519bc06d1046d8eab734325bbc0183deac
AL2327 <al2327@gmail.com>
2017-11-03 11:50:03 -0500

    Update Navigation_Stable.ino
    Added more code to start up.  Cleaned up some lines and added some commenting.

--- Navigation_Stable.ino
+++ Navigation_Stable.ino
@@ -88,13 +88,14 @@ Task t2(500, TASK_FOREVER, &t2Callback, &runner, true);  //adding task to steer 
 Task t3(5000, TASK_FOREVER, &t3Callback, &runner, true); //adding task to talk to fona
 
 void t1Callback() {
-  Serial.print("IMU/Steering Collection:");
+  Serial.print("IMU Collection:");
   Serial.println(millis());
   getIMU();
-  Steering(courseToWaypoint);
 }
 
 void t2Callback() {
+  Serial.print("Steering PID/Throttle Setting:");
+  Serial.println(millis());
   Steering(courseToWaypoint);
   Motor(THRT);
 }
@@ -106,28 +107,67 @@ void t3Callback() {
 void setup()
 {
 
-  Serial.begin(115200);
-  ss.begin(9600);
+  Serial.begin(115200);             //Serial connection to USB->Computer Serial Monitor
+  ss.begin(9600);                   //Serial connection to GPS
 
+  Serial.println("Program Begin..."); 
+  delay(500);
+  
   /* Initialise the imu sensors */
+  Serial.println("IMU Initialise..."); 
+  delay(500);  
   initSensors();
+  
+  /* Initialise Waypoint Array */
+  Serial.println("WAYPOINT Setup"); 
+  delay(500);
+  Waypoint(); 
 
-  Serial.println("Program Begin...");//
-  runner.startNow();  //set point-in-time for scheduling start.
-
-  Waypoint(); //Call waypoint setup.
-
+   /* Initialise Rudder */
   Rudder.attach(35);  // attaches the servo on pin 35 to the servo object
 
   Rudder.write(pos);  //send rudder to mid position
+  
+  Serial.println("Rudder Amidships.  3 Second Hold."); 
   delay(3000);      // wait for 3 seconds after rudder moves.
-
+  
+  Serial.println("Rudder Sweep in 3 seconds. KEEP CLEAR!"); 
+  delay(3000); 
+  
+  for (pos = 45; pos <= 135; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    Rudder.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 135; pos >= 45; pos -= 1) { // goes from 180 degrees to 0 degrees
+    Rudder.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  pos=90;
+  Rudder.write(pos);  //send rudder to mid position
+  delay(1000); 
+  
+  
+  /* Initialize Throttle*/
+  Serial.println("Throttle Setup"); 
+  delay(500);  
   Throttle.attach(36); //attatch throttle to pin 36 to the servo object.
   Throttle.write(THRT); //send command to set throttle to 0.
+  Serial.println("Throttle Set to ZERO"); 
 
+  /* Initialize Steering PID*/
+  Serial.println("Steering PID Set to AUTOMATIC"); 
+  delay(500);  
   PIDRudder.SetMode(AUTOMATIC);
 
+  /* Initialize Fona */
+  Serial.println("Steering PID Set to AUTOMATIC");   
   fonasetup();
+  
+  /* Initialize Scheduler */
+  Serial.println("Task Handler Start Time Set: ");  
+  runner.startNow();  //set point-in-time for scheduling start.
+  Serial.println(millis());
 
 }
 
