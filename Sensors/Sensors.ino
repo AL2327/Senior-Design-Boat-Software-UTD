d07dc0b71e02bf62d431923c0fa7814d3229b0b6
AL2327 <al2327@gmail.com>
2017-11-05 09:39:34 -0600

    Add files via upload

--- Sensors/Sensors.ino
+++ Sensors/Sensors.ino
@@ -1,28 +1,32 @@
 /********************************************************************/
 // First we include the libraries
-#include <OneWire.h>            //onewire communication protocal for waterproof sensor
-#include <DallasTemperature.h>  //library for our waterproof temp sensor that hates us.
 #include <Sensirion.h>       //library for our air temp/humidity sensor
 #include <EasyTransfer.h>    //allows serial communication between microcontrollers
-
 /********************************************************************/
-// Data wire is plugged into pin 2 on the Arduino
-#define ONE_WIRE_BUS 2
 
 //Definitions for bilge pump
 #define Water_Level_Sensor 5
-#define Pump A1
+#define Pump 9
 
 // Definition for voltage sensor input
 #define VoltageSense A1
 
 // Definition for salinity sensor input
-#define salinity A0
+#define salinity 14
 
 //Pin declarations for Sensirion temp sensor
 const uint8_t dataPin  =  4;
 const uint8_t clockPin =  3;
 
+//Variables for NTC Thermristor
+byte NTCPin = A2;
+#define SERIESRESISTOR 10000
+#define NOMINAL_RESISTANCE 10000
+#define NOMINAL_TEMPERATURE 25
+#define BCOEFFICIENT 3950
+float ADCvalue;
+float Resistance;
+float steinhart;
 
 //variables for temperature and salinity sensors
 float temperature = 0;
@@ -42,17 +46,13 @@ float R1 = 100000;
 float R2 = 10000;
 float Vread = 0;
 float Vin = 0.0;
+
+
+
 /********************************************************************/
-// Setup a oneWire instance to communicate with any OneWire devices
-// (not just Maxim/Dallas temperature ICs)
-OneWire oneWire(ONE_WIRE_BUS);
 Sensirion tempSensor = Sensirion(dataPin, clockPin);
 
 /********************************************************************/
-// Pass our oneWire reference to Dallas Temperature.
-DallasTemperature sensors(&oneWire);
-/********************************************************************/
-
 //create serial communication object
 EasyTransfer ET;
 
@@ -90,9 +90,6 @@ void setup(void)
   ET.begin(details(sensorData), &Serial);
 
 
-  // Start up the library
-  sensors.begin();
-
   //initialize pins
   pinMode(Water_Level_Sensor, INPUT);
   pinMode(Pump, OUTPUT);
@@ -105,20 +102,28 @@ void setup(void)
 void loop(void)
 {
 
-  // call sensors.requestTemperatures() to issue a global temperature
-  // request to all devices on the bus
+  //Read and calculate analog NTC thermisitor
+  ADCvalue = analogRead(NTCPin);
+  Serial.print("Analog value ");
+  Serial.print(ADCvalue);
+  Serial.print(" = ");
+  //convert this ADC value into resistance
+  Resistance = (1023 / ADCvalue) - 1;
+  Resistance = SERIESRESISTOR / Resistance;
+  Serial.print(Resistance);
+  Serial.println(" Ohm");
+  //calculate temperature using steinhart formula
+  steinhart = Resistance / NOMINAL_RESISTANCE; // (R/Ro)
+  steinhart = log(steinhart); // ln(R/Ro)
+  steinhart /= BCOEFFICIENT; // 1/B * ln(R/Ro)
+  steinhart += 1.0 / (NOMINAL_TEMPERATURE + 273.15); // + (1/To)
+  steinhart = 1.0 / steinhart; // Invert
+  steinhart -= 273.15; // convert to C
 
-  /********************************************************************/
-  Serial.print(" Requesting temperatures...");
-  sensors.requestTemperatures(); // Send the command to get temperature readings
-  Serial.println("DONE");
-  /********************************************************************/
-
-  Serial.print("Water Temperature is: ");
-  Serial.print(sensors.getTempCByIndex(0)); // Why "byIndex"?
-  // You can have more than one DS18B20 on the same bus.
-  // 0 refers to the first IC on the wire
-
+  Serial.print("Temperature ");
+  Serial.print(steinhart);
+  Serial.println(" oC");
+  //end thermristor calculations and output
 
   //Read Sensirion sensor
   tempSensor.measure(&temperature, &humidity, &dewpoint);
@@ -129,12 +134,13 @@ void loop(void)
   Serial.print(humidity);
   Serial.print(" %, Dewpoint: ");
   Serial.print(dewpoint);
-  Serial.println(" C");
+  Serial.print(" C");
 
   //Read salinity sensor
   SalReading = analogRead(salinity);
   float sal = SalReading * (5.0 / 1023.0);
-  Serial.print("Salinity: ");
+  Serial.println(" ");
+  Serial.println("Salinity: ");
   Serial.print( sal);
   Serial.println(" ppt");
 
@@ -143,7 +149,7 @@ void loop(void)
 
   //set pump to on/off according to float switch state
   if (FloatSwitch == HIGH) {
-    Serial.println( "WATER LEVEL - LOW");
+  Serial.println( "WATER LEVEL - LOW");
     digitalWrite(Pump, LOW);
   }
   else {
@@ -157,27 +163,27 @@ void loop(void)
   //Do some ENA Math and output voltage.
   Vout = (Vread * 5.0) / 1024;
   Vin = Vout / (R2 / (R1 + R2));
-  Serial.print("Input Voltage = ");
+  Serial.println("Input Voltage = ");
   Serial.print(Vin);
-  Serial.print("");
+  Serial.println("");
 
   //assign a battery state of charge number depending on the Vin.  We can use this number to limit certain functions based on the battery's current remaining capacity.
 
   if (Vin > 12.50)
-  {
-    BatterySOC = 1;   //our battery is "fully charged"  >80% State of Charge
-  }
-  else if (Vin <= 12.49 && Vin > 11.96) {
-    BatterySOC = 2; // our battery is in its normal operating range 40-80% Charge
-  }
-  else if (Vin <= 11.95 && Vin > 11.66) {
-    BatterySOC = 3; // our battery is low.  It is between 20-40% charged
-  }
-  else if (Vin <= 1165) {
-    BatterySOC = 4; // our battery is under 20% charged. This is critically low.
-  }
+{
+  BatterySOC = 1;   //our battery is "fully charged"  >80% State of Charge
+}
+else if (Vin <= 12.49 && Vin > 11.96) {
+  BatterySOC = 2; // our battery is in its normal operating range 40-80% Charge
+}
+else if (Vin <= 11.95 && Vin > 11.66) {
+  BatterySOC = 3; // our battery is low.  It is between 20-40% charged
+}
+else if (Vin <= 1165) {
+  BatterySOC = 4; // our battery is under 20% charged. This is critically low.
+}
 
-  ET.sendData();   //Send all our data over the serial port to teensy
+ET.sendData();   //Send all our data over the serial port to teensy
 
 
 }
