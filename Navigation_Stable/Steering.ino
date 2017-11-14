39288600e31f5f8150104606ab5d40dbd44aba63
AL2327 <al2327@gmail.com>
2017-11-14 11:32:32 -0600

    Steering
    Steering changed from PID to simple "bang-bang" for testing.

--- Navigation_Stable/Steering.ino
+++ Navigation_Stable/Steering.ino
@@ -17,12 +17,71 @@ void Steering(double courseToWaypoint) {
   Serial.println("");
 
 
-  //Serial.println("RUDDER COMMAND: "); //
-  //Serial.println("Commanded Rudder Posistion: ");
-  //Serial.print(pos);
-  //Serial.println("");
 
-  /*STEERING CODE*/
+
+if(courseError <= -149 && courseError >= -179)
+{
+        pos = 150;        //make it turn full left
+}
+
+else if(courseError >= -148 && courseError <=-100)
+{
+              pos=pos+30;
+}
+else if(courseError >= -99  && courseError <= -30)
+{
+              pos=pos+15;
+}
+
+else if(courseError >=-29 && courseError <=-10)
+{
+              pos=pos+10;
+}
+
+else if(courseError >=9 && courseError <0)
+{
+              pos=pos+3;
+}
+
+/*Other direction*/
+if(courseError >= 149 && courseError <= 179)
+{
+        pos = 150;        
+}
+
+else if(courseError <= 148 && courseError >=100)
+{
+        pos=pos-30;
+}
+else if(courseError <= 99  && courseError >= 30)
+{
+        pos=pos-15;
+}
+
+else if(courseError <=29 && courseError >=10)
+{
+        pos=pos-10;
+}
+
+else if(courseError <=9 && courseError >0)
+{
+        pos=pos-3;
+}
+
+else if(courseError == 0){
+    pos=90;
+}
+
+pos=constrain(pos, 30, 150);
+Rudder.write(pos);
+
+  Serial.println("RUDDER COMMAND: "); //
+  Serial.println("Commanded Rudder Posistion: ");
+  Serial.print(pos);
+  Serial.println("");
+
+  
+/*  /*STEERING CODE
   PIDRudder.Compute();          // PID computation
   pos = map(pidoutput, 0, 255, 0, 180);  //map PID output(double) to pos(integer) range
   pos = constrain(pos, 45, 135);        //contrain the rudder to +/- 30 deg.
